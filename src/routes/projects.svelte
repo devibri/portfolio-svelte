5889b9d0dbcd3792024d9155d6086e3bbaed70c6
Devi Acharya <devi@devi.rocks>
2026-09-03 20:20:58 -0400

    change chip color

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -259,7 +259,7 @@
 					<footer class="card-footer mt-auto">
 						<div class="cardbuttoncontainer">
 							{#each getProjectLinks(project) as link}
-								<a href={link.href} type="button" class="btn variant-filled cardbutton">
+								<a href={link.href} type="button" class="btn variant-soft-primary cardbutton">
 									<span>{link.label}</span>
 								</a>
 							{/each}
